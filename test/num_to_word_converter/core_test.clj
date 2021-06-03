26bac6d1ac78aa9c0d3bbd74e2ad846b5adbd8c2
RayceeM <mwatelaraycee33@gmail.com>
2021-06-03 10:49:04 +0300

    Add test directory

---
+++ test/num_to_word_converter/core_test.clj
@@ -0,0 +1,7 @@
+(ns num-to-word-converter.core-test
+  (:require [clojure.test :refer :all]
+            [num-to-word-converter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
