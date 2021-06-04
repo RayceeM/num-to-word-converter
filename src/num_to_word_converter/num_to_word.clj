cc25417d5b67b8016d4b3c4130e251865d4b0c7d
RayceeM <mwatelaraycee33@gmail.com>
2021-06-04 14:53:07 +0300

    Fix num->digit-count function

--- src/num_to_word_converter/num_to_word.clj
+++ src/num_to_word_converter/num_to_word.clj
@@ -36,40 +36,41 @@
 (defn num->digits-count
   "Converts a number to a digit and gives the count"
   [num]
-  (->> (str num)
-       seq
-       (map str)
-       (map read-string)
+  (->> num
+       (str)
+       (map int)
+       (map #(- % (int \0)))
+       (into [])
        count))
 
 (defn get-quotient [{:keys [number divisor remainder]}]
-  (if divisor
-    (quot number divisor)
-    (rem num remainder)))
+  (cond
+    divisor (quot number divisor)
+    remainder (rem number remainder)
+    :else number))
 
 (defn number-to-words
   "Divide number if greater than nineteen"
   [num]
   (if (> num 19)
-    (str (get multiples-of-ten (get-quotient  {:number num
-                                               :divisor 10}))
-         (get single-numbers (get-quotient  {:number num
-                                             :reminder 10})))
+    (str (get multiples-of-ten (get-quotient {:number num
+                                              :divisor 10}))
+         (get single-numbers (get-quotient {:number num
+                                             :remainder 10})))
     (get single-numbers num)))
 
 (defn convert-number-to-words
   [num]
-  (let [number-count (num->digits-count num)
-        _ (prn "num-count: " number-count)]
+  (let [number-count (num->digits-count num)]
     (case number-count
       (or 1 2) (number-to-words num)
       3  (str (get single-numbers (get-quotient {:number num
                                                  :divisor 100}))
               " hundred and "
               (number-to-words (get-quotient  {:number num
-                                               :reminder 100})))
+                                               :remainder 100})))
       4  (str (get single-numbers (get-quotient {:number num
                                                  :divisor 1000}))
               " thousand "
               (number-to-words  (get-quotient {:number num
-                                               :reminder 1000}))))))
+                                               :remainder 1000}))))))
